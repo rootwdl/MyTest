f4371a5d6d6182626f6e3419830c7a4c8d845fb7
王东来 <1791820472@qq.com>
2022-05-17 19:58:52 +0800

    第一次提交本地仓库

---
+++ src/com/www/Test01.java
@@ -0,0 +1,19 @@
+package com.www;/*
+ *@ClassName: Test01
+ *@Package: com.www
+ *@Description: ..
+ *
+ *@date: 2022/5/17 19:50
+ *@author: 南邮刘华强
+ */
+
+public class Test01 {
+
+    int x;
+    int y;
+
+    public static void main(String[] args) {
+        System.out.println("Hello,GitHub");
+    }
+
+}
